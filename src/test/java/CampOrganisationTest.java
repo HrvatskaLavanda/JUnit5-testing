64d02a296630eeac6bd14a1102be6b51fdc097cf
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-18 17:01:10 +0200

    (feat) add unit test for addStudent() method when first parameter "newStudentFirstName" is null

---
+++ src/test/java/CampOrganisationTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CampOrganisationTest {
+
+    @Test
+    public void shouldThrowExceptionWhenStudentFirstNameIsNull() {
+        //given
+        CampOrganisation campOrganisation = new CampOrganisation(new ArrayList<>());
+        String newStudentLastName = "Nowak";
+
+        //when then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(null, newStudentLastName));
+
+    }
+
+}
