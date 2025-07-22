25a5fa2d9ecebca91f4821056c4bbab92ba2ffda
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-22 13:35:24 +0200

    (refactor) parametrize the tests for all null scenarios

--- src/test/java/CampOrganisationTest.java
+++ src/test/java/CampOrganisationTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,22 +23,36 @@ class CampOrganisationTest {
         campOrganisation = new CampOrganisation(new ArrayList<>());
     }
 
-    @Test
-    public void shouldThrowExceptionWhenStudentFirstNameIsNull() {
-        //given
-        String newStudentLastName = "Nowak";
+//    @Test
+//    public void shouldThrowExceptionWhenStudentFirstNameIsNull() {
+//        //given
+//        String newStudentLastName = "Nowak";
+//
+//        //when then
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(null, newStudentLastName));
+//    }
 
-        //when then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(null, newStudentLastName));
+//    @Test
+//    public void shouldThrowExceptionWhenStudentLastNameIsNull() {
+//        //given
+//        String newStudentFirstName = "Basia";
+//
+//        //when then
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, null));
+//    }
+
+    private static Stream<Arguments> allNullExamples() {
+        return Stream.of(
+                Arguments.of(null, "Nowak"),
+                Arguments.of("Basia", null),
+                Arguments.of(null, null)
+        );
     }
 
-    @Test
-    public void shouldThrowExceptionWhenStudentLastNameIsNull() {
-        //given
-        String newStudentFirstName = "Basia";
-
-        //when then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, null));
+    @ParameterizedTest
+    @MethodSource("allNullExamples")
+    public void shouldThrowExceptionWhenStudentAnyNameIsNull(String firstName, String lastName) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(firstName, lastName));
     }
 
     @Test
