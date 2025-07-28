9b2b0b1140ce87314c37aaf8a9c691a7489f61b8
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-28 21:25:26 +0200

    (feat) add an assertion to verify that the file exists

--- src/test/java/neverland/ReportTest.java
+++ src/test/java/neverland/ReportTest.java
@@ -1,6 +1,10 @@
 package neverland;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileWriter;
 
 class ReportTest {
 
@@ -10,11 +14,12 @@ class ReportTest {
         Tax tax = new SingleWIthNoKids();
         var citizen = new Citizen(CitizenTest.SALARY, tax);
         Official official = new Official();
+        File file = new File("report.txt");
 
         //when
         official.generateAnnualReport(citizen);
 
         //then
-
+        Assertions.assertTrue(file.exists());
     }
 }
