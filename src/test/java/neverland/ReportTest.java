62b9924ea28add4020d0c75e1e68a02c1736fb5a
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-07-31 23:01:49 +0200

    (fix) change file that creates test report, extract to method all the assertions, change exception from try-catch to throws exception structure

--- src/test/java/neverland/ReportTest.java
+++ src/test/java/neverland/ReportTest.java
@@ -4,36 +4,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
 class ReportTest {
 
+    File testFile = new File("test-report.txt");
+
     @Test
-    public void shouldGenerateReport() {
+    public void shouldGenerateReport() throws IOException {
         //given
         Tax tax = new SingleWIthNoKids();
         var citizen = new Citizen(CitizenTest.SALARY, tax);
-        Official official = new Official();
-        File file = new File("report.txt");
+        Official official = new Official(testFile.getPath());
 
         //when
         official.generateAnnualReport(citizen);
 
         //then
-        Assertions.assertTrue(file.exists());
+        Assertions.assertTrue(testFile.exists());
+        String report = Files.readString(testFile.toPath());
 
-        String report;
-        try {
-            report = Files.readString(file.toPath());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        assertTrue(citizen, report);
+    }
+
+    public void assertTrue(Citizen citizen, String report) {
         Assertions.assertTrue(report.contains("Name: " + citizen.getName()));
         Assertions.assertTrue(report.contains("Surname: " + citizen.getSurname()));
         Assertions.assertTrue(report.contains("Annual income: " + citizen.calculateAnnualIncome()));
         Assertions.assertTrue(report.contains("Tax to pay: " + citizen.calculateTaxToPay()));
-
     }
 }
